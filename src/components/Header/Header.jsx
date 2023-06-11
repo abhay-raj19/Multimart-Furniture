7375bbb361f0d4f256e1ed0e97c519176b60dcb3
abhay-raj19 <rajpootabhay423@gmail.com>
2023-06-11 20:10:58 +0530

    navigation banyi hai

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,79 @@
-
-
 import React from 'react'
+
+import { NavLink } from 'react-router-dom'
+import './header.css'
+
+import { motion } from 'framer-motion'
+
+import logo from '../../assets/images/eco-logo.png'
+import userIcon from '../../assets/images/user-icon.png'
+
+import { Container,NavItem,Row } from 'reactstrap'
+
+const nav_links  = [
+    {
+        path:'home',
+        display:'Home'
+    },
+    {
+        path:'shop',
+        display:'Shop'
+    },
+    {
+        path:'cart',
+        display:'Cart'
+    },
+];
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header">
+        <Container>
+            <Row>
+                <div className="nav__wrapper">
+                    <div className="logo">
+                        <img src={logo} alt="Logo" />
+                        <div>
+                            <h1>MultiMart</h1>
+                            
+                        </div>
+                    </div>
+                    <div className="navigation">
+                        <ul className="menu">
+                        {nav_links.map((item,index) =>(
+                            <li className="nav__item" key={index}>
+                                <NavLink
+                                to={item.path} 
+                                className={(navClass)=>
+                                    navClass.isActive ? "nav__active" : ""
+                                    }
+                                >
+                                {item.display}
+                            </NavLink>
+                        </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="nav__icons">
+                        <span className="fav__icon"><i class="ri-heart-line"></i>
+                        <span className="badge">4</span>
+                        </span>
+                        <span className="cart__icon"><i class="ri-shopping-bag-line"></i>
+                        <span className="badge">2</span>
+                        </span>
+                        <span><motion.img whileTap={{scale:1.5}} src={userIcon} alt=''/></span>
+                    </div>
+
+                    <div className="mobile__menu">
+                        <span>
+                            <i class="ri-menu-line"></i>
+                        </span>
+                    </div>
+
+                </div>
+            </Row>
+        </Container>
+    </header>
   )
 }
 
--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,4 +1,4 @@
-import { Routes,Route } from 'react-router-dom'
+import { Routes,Route,Navigate } from 'react-router-dom'
 
 
 import Home from '../pages/Home'
@@ -11,6 +11,7 @@ import Signup from '../pages/Signup'
 
 const Routers = () => {
   return <Routes>
+    <Route path='/' element={<Navigate to='/'/>}/>
     <Route path='home' element={<Home/>}/>
     <Route path='shop' element={<Shop/>}/>
     <Route path='cart' element={<Cart/>}/>
