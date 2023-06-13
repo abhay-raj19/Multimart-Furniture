507502603739c2512feaa110fcbb3ca989fcf10f
abhay-raj19 <rajpootabhay423@gmail.com>
2023-06-13 22:51:17 +0530

    added minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Helmet from '../components/Helmet/Helmet'
 
 import { Container,Row,Col } from 'reactstrap';
+import heroImg from "../assets/images/hero-img.png"
 
 const Home = () => {
 
@@ -12,14 +13,20 @@ const Home = () => {
     <section className="hero__section">
         <Container>
           <Row>
-            <col lg='6' md='6'>
+            <Col lg='6' md='6'>
               <div className="hero__content">
                 <p className="hero__subtitle">Trending product in {year}</p>
                 <h2>Make Your Interior More Minimalistic & Modern</h2> 
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum unde earum, numquam nobis esse dolorum, eaque quo eum perferendis dolore error laudantium ad est iste! Ipsum fugit magnam ipsa itaque?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum unde earum, numquam nobis esse dolorum, eaque quo eum perferendis dolore error laudantium ad est iste! Ipsum fugit magnam ipsa itaque?
+                </p>
+                <button className="buy__btn">SHOP NOW</button>
               </div>
-            </col>
-
+            </Col>
+            <Col lg='6' md='6'>
+              <div className="hero__img">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
           </Row>
         </Container>
 
