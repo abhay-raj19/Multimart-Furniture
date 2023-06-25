4488fb1961bb94aea49e17b394cab294e8e87ec5
abhay-raj19 <rajpootabhay423@gmail.com>
2023-06-25 09:26:25 +0530

    adding logout funtionallity

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,121 +1,142 @@
-import React, { useRef,useEffect } from 'react'
+import React, { useRef, useEffect } from "react";
 
-import { Link, NavLink, Navigate, useNavigate } from 'react-router-dom'
-import './header.css'
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import "./header.css";
 
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 
-import logo from '../../assets/images/eco-logo.png'
-import userIcon from '../../assets/images/user-icon.png'
+import logo from "../../assets/images/eco-logo.png";
+import userIcon from "../../assets/images/user-icon.png";
 
-import { Container, Row } from 'reactstrap';
-import { useSelector } from 'react-redux'
+import { Container, Row } from "reactstrap";
+import { useSelector } from "react-redux";
+import useAuth from "../../custom-hooks/useAuth";
 
 const nav_links = [
-    {
-        path: 'home',
-        display: 'Home'
-    },
-    {
-        path: 'shop',
-        display: 'Shop'
-    },
-    {
-        path: 'cart',
-        display: 'Cart'
-    },
+  {
+    path: "home",
+    display: "Home",
+  },
+  {
+    path: "shop",
+    display: "Shop",
+  },
+  {
+    path: "cart",
+    display: "Cart",
+  },
 ];
 
 const Header = () => {
+  const headerRef = useRef(null);
+  const totalQuantity = useSelector((state) => state.cart.totalQuantity);
+  const profileActionRef = useRef(null);
+  const menuRef = useRef(null);
+  const navigate = useNavigate();
+  const { currentUser } = useAuth();
+  //   console.log(currentUser.photoURL);
 
-    const headerRef = useRef(null)
-    const totalQuantity = useSelector(state => state.cart.totalQuantity)
-
-    const menuRef = useRef(null);
-    const navigate = useNavigate();
-
-    const stickyHeaderFunc = () => {
-        window.addEventListener("scroll", () => {
-            if (
-                document.body.scrollTop >80 ||
-                document.documentElement.scrollTop > 80
-            ) {
-                headerRef.current.classList.add("sticky__header")
-            } else {
-                headerRef.current.classList.remove("sticky__header")
-            }
-        });
-
-    };
-
-    useEffect(()=>{
-       stickyHeaderFunc()
-       
-       
-       return () => window.removeEventListener("scroll", stickyHeaderFunc );
+  const stickyHeaderFunc = () => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky__header");
+      } else {
+        headerRef.current.classList.remove("sticky__header");
+      }
     });
+  };
 
-    const menuToggle = () => menuRef.current.classList.toggle('activr__menu')
+  useEffect(() => {
+    stickyHeaderFunc();
 
+    return () => window.removeEventListener("scroll", stickyHeaderFunc);
+  });
 
-    const navigateToCart = () => {
-        navigate('/cart');
-    };
+  const menuToggle = () => menuRef.current.classList.toggle("active__menu");
 
+  const navigateToCart = () => {
+    navigate("/cart");
+  };
 
+  const toggleProfileActions = () =>
+    profileActionRef.current.classList.toggle("show__profileActions");
 
-
-
-    return (
-        <header className="header" ref={headerRef}>
-            <Container>
-                <Row>
-                    <div className="nav__wrapper">
-                        <div className="logo">
-                        <Link to="/home"><img src={logo} alt="Logo" /></Link>                            
-                            <div>
-                                <Link to='/home'><h1>Marketopolis</h1></Link>
-
-                            </div>
-                        </div>
-                        <div className="navigation" ref={menuRef} onClick={menuToggle}>
-                            <ul className="menu">
-                                {nav_links.map((item, index) => (
-                                    <li className="nav__item" key={index}>
-                                        <NavLink
-                                            to={item.path}
-                                            className={(navClass) =>
-                                                navClass.isActive ? "nav__active" : ""
-                                            }
-                                        >
-                                            {item.display}
-                                        </NavLink>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                        <div className="nav__icons">
-                            <span className="fav__icon"><i class="ri-heart-line"></i>
-                                <span className="badge">4</span>
-                            </span>
-                            <span className="cart__icon" onClick={navigateToCart}><i class="ri-shopping-bag-line"></i>
-                                <span className="badge">{totalQuantity}</span>
-                            </span>
-                            <span><motion.img whileTap={{ scale: 1.5 }} src={userIcon} alt='' /></span>
-                            <div className="mobile__menu">
-                            <span onClick={menuToggle}>
-                                <i class="ri-menu-line"></i>
-                            </span>
-                        </div>
-                        </div>
-
-                        
-
+  return (
+    <header className="header" ref={headerRef}>
+      <Container>
+        <Row>
+          <div className="nav__wrapper">
+            <div className="logo">
+              <Link to="/home">
+                <img src={logo} alt="Logo" />
+              </Link>
+              <div>
+                <Link to="/home">
+                  <h1>Marketopolis</h1>
+                </Link>
+              </div>
+            </div>
+            <div className="navigation" ref={menuRef} onClick={menuToggle}>
+              <ul className="menu">
+                {nav_links.map((item, index) => (
+                  <li className="nav__item" key={index}>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) =>
+                        navClass.isActive ? "nav__active" : ""
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="nav__icons">
+              <span className="fav__icon">
+                <i class="ri-heart-line"></i>
+                <span className="badge">4</span>
+              </span>
+              <span className="cart__icon" onClick={navigateToCart}>
+                <i class="ri-shopping-bag-line"></i>
+                <span className="badge">{totalQuantity}</span>
+              </span>
+              <div className="profile">
+                <motion.img
+                  whileTap={{ scale: 1.5 }}
+                  src={currentUser ? currentUser.photoURL : userIcon}
+                  alt=""
+                  onClick={toggleProfileActions}
+                />
+                <div
+                  className="profile__actions"
+                  ref={profileActionRef}
+                  onClick={toggleProfileActions}
+                >
+                  { currentUser ? (
+                    <span> Logout </span>
+                  ) : (
+                    <div>
+                      <Link to="/signup">Signup</Link>
+                      <Link to="/login">Login</Link>
                     </div>
-                </Row>
-            </Container>
-        </header>
-    )
-}
+                  )}
+                </div>
+              </div>
+              <div className="mobile__menu">
+                <span onClick={menuToggle}>
+                  <i class="ri-menu-line"></i>
+                </span>
+              </div>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
